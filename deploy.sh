c9a7578a9e5f5a75b341bd064b3e7561c9915601
Eduardo Borsa <borsa.dado@gmail.com>
2020-12-01 08:11:08 -0300

    Automated deploy script
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "starting deploy process"
+git remote add gigalixir https://borsa.dado%40gmail.com:624f88ba-6c4b-44a0-96d4-c7d89f9e2ac0@git.gigalixir.com/elixir-pro-blog.git
+git push gigalixir master
+echo "finished"
